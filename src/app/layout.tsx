db9b5dcb5a2cac9aac744440089227a946b3e941
umvelkior <bareilhe@et.esiea.fr>
2025-03-03 01:36:43 +0100

    16min 42s création de l'identification avec clerk.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="fr">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
