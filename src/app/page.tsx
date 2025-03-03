470f205dca132f1b56c321307e8b9a677870e472
umvelkior <bareilhe@et.esiea.fr>
2025-03-03 16:59:25 +0100

    25min 40s : dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>COUCOU</h1>
+    <div className="m-4">
+      <SignedOut>
+            <SignInButton mode="modal">
+              <Button>
+                Sign in
+              </Button>
+            </SignInButton>
+      </SignedOut>
+      <SignedIn>
+              <UserButton />
+      </SignedIn>
     </div>
   );
 }
