04382e4a009c3aac6c9c498f0f2639c1ee3e45ca
umvelkior <bareilhe@et.esiea.fr>
2025-03-03 23:52:39 +0100

    maj user.actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           <div className="max-w-7xl mx-auto px-4">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                 <div className="hidden lg:block lg:col-span-3">
-                  Sidebar
+                  <Sidebar/>
                 </div>
                 <div className="lg:col-span-9">{children}</div>
               </div>
